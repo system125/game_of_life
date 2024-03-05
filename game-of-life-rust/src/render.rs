76ebd9de6c08732741735a52f1ca7c3cb61fa2e3
Sanjeev Sapkota <hundredtwentyfive25@gmail.com>
2024-03-05 18:45:37 +0545

    implemented grid for game

--- game-of-life-rust/src/render.rs
+++ game-of-life-rust/src/render.rs
@@ -1,12 +1,32 @@
 
+use wasm_bindgen::prelude::*;
 use web_sys::CanvasRenderingContext2d;
 
+
+use crate::consts::{CELL_SIZE, STROKE_COLOR};
 /**
  * Module to help render game of life board
  **/
 
  use crate::universe::Universe;
  
- fn render_grid(cnts:&CanvasRenderingContext2d,uni:&Universe){
+ pub fn render_grid(context:&CanvasRenderingContext2d,uni:&Universe){
+    context.begin_path();
+    context.set_stroke_style(&JsValue::from_str(STROKE_COLOR));
 
+    for i in 0..=uni.width() {
+        let start_x = (i * (CELL_SIZE + 1) + 1) as f64;
+        let end_y =  (uni.height()*(CELL_SIZE + 1) + 1) as f64;
+        
+        context.move_to(start_x.into(), 0.0);
+        context.line_to(start_x,end_y)
+    }
+
+    for j in 0..=uni.height() {
+        let start_y = (j * (CELL_SIZE + 1) + 1) as f64;
+        let end_x = (uni.width()*(CELL_SIZE + 1)+1 ) as f64;
+
+        context.move_to(0.0, start_y.into());
+        context.line_to(end_x, start_y);
+    }
  }
