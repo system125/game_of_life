35c5e06f53443bd6a18f5452ef2995a5954808e9
Sanjeev Sapkota <hundredtwentyfive25@gmail.com>
2024-03-05 19:31:32 +0545

    made dead darker

--- game-of-life-rust/src/consts.rs
+++ game-of-life-rust/src/consts.rs
@@ -3,7 +3,7 @@
 //     pub const CELL_SIZE: u32 = 5;
 // }
 
-pub const CELL_SIZE: u32 = 6;
+pub const CELL_SIZE: u32 = 10;
 pub const STROKE_COLOR: &'static str = "#ffffff";
 pub const ALIVE_COLOR: &'static str = "#133b2d";
-pub const DEAD_COLOR: &'static str = "#0a0a0a";
+pub const DEAD_COLOR: &'static str = "#000000";
