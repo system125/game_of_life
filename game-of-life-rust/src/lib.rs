e1f4a7c4dac791b410fd3565eaa8044e283fdb4a
Sanjeev Sapkota <hundredtwentyfive25@gmail.com>
2024-03-05 14:26:31 +0545

    create logging at base

--- game-of-life-rust/src/lib.rs
+++ game-of-life-rust/src/lib.rs
@@ -1,1 +1,26 @@
 
+use wasm_bindgen::prelude::*;
+
+// #[cfg(feature = "wee_alloc")]
+// #[global_allocator]
+// static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen] 
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+
+    #[wasm_bindgen]
+    fn alert(st: &str);
+}
+
+macro_rules! console_log {
+    // Note that this is using the `log` function imported above during
+    // `bare_bones`
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    console_log!("This hurtz bad!!");
+}
---
+++ index.ts
@@ -0,0 +1,4 @@
+import * as wasm from "game-of-life-rust"
+
+console.log("Running certain scientific railgun");
+wasm.greet();
